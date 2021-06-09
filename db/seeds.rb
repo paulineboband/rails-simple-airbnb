cb77896d0db96d5ca04e6a72895af3832eb0eff7
Pauline Boband <paulineboband@gmail.com>
2021-06-09 16:45:55 +0200

    ex3

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Flat.destroy_all
+
+puts "Creating flats..."
+ny = { name: "NY flat", address: "7 Boundary St, NY E2 7JE", description: "lovely flat", price_per_night: 50, number_of_guests: 3}
+london = { name: "London flat", address: "7 Boundary St, London E2 7JE", description: "lovely flat", price_per_night: 50, number_of_guests: 3}
+dubai = { name: "Dubai flat", address: "7 Boundary St, Dubai E2 7JE", description: "lovely flat", price_per_night: 50, number_of_guests: 3}
+paris = { name: "Paris flat", address: "7 Boundary St, Paris E2 7JE", description: "lovely flat", price_per_night: 50, number_of_guests: 3}
+
+[ london, ny, dubai, paris ].each do |attributes|
+  flat = Flat.create!(attributes)
+  puts "Created #{flat.name}"
+end
+puts "Finished!"
